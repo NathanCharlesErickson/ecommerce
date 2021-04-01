97cabbc6ba6b921e77fa33005373c2af50da4ead
Nathan Erickson <nerickson@linfield.edu>
2021-03-31 20:21:40 -0700

    navbar

--- NathanIanEcomFront/app.tsx
+++ NathanIanEcomFront/app.tsx
@@ -2,11 +2,16 @@ declare var require: any
 
 var React = require('react');
 var ReactDOM = require('react-dom');
+var Router = require('react-router-dom')
+var Home = require('./compoents/Home.tsx')
+
+
 
 export class Hello extends React.Component {
     render() {
         return (
-            <h1>Welcome to our Ecommerce App! Test</h1>
+
+            <h1> hello world </h1>
             );
     }
 }
